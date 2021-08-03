f5713d35fe60b646b76400e5cc8b3ecae662e459
Shin-sibainu <shincode0712@gmail.com>
2021-08-03 21:16:32 +0900

    add Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Home from "./Home";
 import Navbar from "./Navbar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Create from "./Create";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
