8cb9ecd72adffa27c9afe47f715b83054b610105
Shin-sibainu <shincode0712@gmail.com>
2021-08-04 10:20:16 +0900

    completed

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,20 +6,28 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setData(data);
-        setPending(false);
-      })
-      .catch((e) => {
-        console.log(e);
-        setError(e.message);
-      });
-  }, []);
+    const abortCont = new AbortController();
 
+    setTimeout(() => {
+      fetch(url, { signal: abortCont.signal })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setData(data);
+          setPending(false); //受け取ったことの確認でfalseにしてる。
+        })
+        .catch((e) => {
+          if (e.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            setPending(false);
+            setError(e.message);
+          }
+        });
+    }, 1000);
+    return () => abortCont.abort();
+  }, [url]);
   return { data, isPending, error };
 };
 
